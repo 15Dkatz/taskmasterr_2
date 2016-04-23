430541af49dddc7be0885a69246c7df9a912a6c2
davidkatz <15Dkatz@shcp.edu>
2016-04-22 23:52:39 -0700

    change Name works.

--- www/js/account.js
+++ www/js/account.js
@@ -1,4 +1,4 @@
-myApp.controller('AccountCtrl', function($scope, auth, store, $state, $rootScope) {
+myApp.controller('AccountCtrl', function($scope, auth, store, $state, $rootScope, $http) {
   $scope.logout = function() {
     auth.signout();
     store.remove('token');
@@ -10,13 +10,77 @@ myApp.controller('AccountCtrl', function($scope, auth, store, $state, $rootScope
 
   $scope.name;
 
+  var user_id;
+
   $rootScope.$on('userAuthed', function() {
-  	$scope.name = $rootScope.currentUser.profile.nickname;
+  	console.log("userAuthed");
+  	$scope.$apply(function() {
+  		if (!$rootScope.currentUser.profile.user_metadata.nickname) {
+  			$scope.name = $rootScope.currentUser.profile.nickname;
+  		} else {
+  			$scope.name = $rootScope.currentUser.profile.user_metadata.nickname;
+  		}
+  		
+  		user_id = $rootScope.currentUser.profile.user_id;
+
+  		console.log("user_id", user_id);
+  	})
   })
 
   // $scope.name = $rootScope.currentUser.nickname;
 
-  $scope.updateName = function() {
-  	console.log("name", $scope.name);
-  }
+  	$scope.updateName = function(newName) {
+	  	// console.log("name", $scope.name);
+	  	console.log("currentUser", $rootScope.currentUser);
+
+	  	var request = {
+	  		"user_metadata": {
+	        	"nickname": newName
+	        }
+	    }
+	    var update = $http.patch('https://davidjtkatz.auth0.com/api/v2/users/' + user_id, request);
+
+	    update.then(function(response) {
+	            console.log("update success", response);
+	            $scope.response = "success";
+	        }, function(response) {
+	            console.log("update failure", response);
+	            $scope.response = "failure";
+        });
+
+	    setTimeout(function() {
+	    	// console.log("resetting response");
+	    	$scope.$apply(function() {
+	    		$scope.response = "";
+	    	})
+	    }, 5000)
+
+    	$scope.name = newName;
+	}
+
+	$scope.updateEmail = function(newEmail) {
+	  	// console.log("name", $scope.name);
+	  	console.log("currentUser", $rootScope.currentUser);
+
+	  	var request = {
+	  		// "user_metadata": {
+	        	"email": newEmail
+	        // }
+	    }
+	    var update = $http.patch('https://davidjtkatz.auth0.com/api/v2/users/' + user_id, request);
+
+	    update.then(function(response) {
+	            console.log("update success", response);
+	        }, function(response) {
+	            console.log("update failure", response);
+        });
+
+        $scope.response = response;
+    	// $scope.name = newName;
+	}
+
+
 });
+
+// have the response message fadeAway
+// update auth0 appearance and coloring.
